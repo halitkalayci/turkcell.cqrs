05b0f3a439713b9e8f91e846d9fbe23899c64602
halitklyc <halitenes.kalayci@gmail.com>
2025-01-29 18:54:40 +0300

    add spring security

---
+++ src/main/java/com/turkcell/turkcellcqrs/core/security/JwtFilter.java
@@ -0,0 +1,57 @@
+package com.turkcell.turkcellcqrs.core.security;
+
+import com.turkcell.turkcellcqrs.core.jwt.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter
+{
+    private final JwtService jwtService;
+
+    public JwtFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        String jwtHeader = request.getHeader("Authorization");
+
+        if(jwtHeader != null && jwtHeader.startsWith("Bearer ")) {
+            String jwt = jwtHeader.substring(7);
+            String username = jwtService.extractUsername(jwt);
+            List<String> roles = jwtService.extractRoles(jwt);
+
+            List<GrantedAuthority> authorities = roles
+                    .stream()
+                    .map(role -> (GrantedAuthority) () -> role)
+                    .toList();
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    username,
+                    null,
+                    authorities
+            );
+
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+
+        filterChain.doFilter(request,response);
+    }
+}
---
+++ src/main/java/com/turkcell/turkcellcqrs/core/security/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.turkcell.turkcellcqrs.core.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfiguration {
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfiguration(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
